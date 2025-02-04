e53b774f5d4c2e52a223466ec036b66489dbb928
jack ning <github@bytedesk.com>
2025-02-04 15:49:39 +0800

    sync: desktop update 2025-02-05 07:58:13

--- src/hooks/useStyle.tsx
+++ src/hooks/useStyle.tsx
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-04-02 11:20:54
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-12-23 10:34:04
+ * @LastEditTime: 2025-02-05 07:48:30
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM –
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -30,6 +30,7 @@ function useStyle() {
     // background: isDarkMode ? "#141414" : "#fff",
     borderBottom: isDarkMode ? "1px solid #333" : "1px solid #ccc",
     background: isDarkMode ? "#141414" : "#eee", // f5f5f5
+    // height: 40,
   };
   const rightSiderStyle: React.CSSProperties = {
     borderLeft: isDarkMode ? "1px solid #333" : "1px solid #ccc",
