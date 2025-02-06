6be98d9479a7afed710665f044999a94788a3eeb
jack ning <github@bytedesk.com>
2025-02-06 12:43:45 +0800

    sync: desktop update 2025-02-06 15:49:31

--- src/apis/ticket/ticket.ts
+++ src/apis/ticket/ticket.ts
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-02-06 11:07:09
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-05 16:18:06
+ * @LastEditTime: 2025-02-06 15:49:32
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM –
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -57,8 +57,8 @@ export async function updateTicket(ticket: TICKET.TicketRequest) {
   });
 }
 
-export async function dismissTicket(uid: string) {
-  return request<TICKET.HttpResult>("/api/v1/ticket/dismiss", {
+export async function deleteTicket(uid: string) {
+  return request<TICKET.HttpResult>("/api/v1/ticket/delete", {
     method: "POST",
     data: {
       uid,
