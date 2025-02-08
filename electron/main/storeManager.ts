ac65ff67b8cc006c08b592d156fa361bb77b7657
jack ning <github@bytedesk.com>
2025-02-08 11:03:32 +0800

    sync: desktop update 2025-02-08 15:36:58

--- electron/main/storeManager.ts
+++ electron/main/storeManager.ts
@@ -1,9 +1,8 @@
-
 /*
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-01-20 21:15:45
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2024-11-26 11:16:25
+ * @LastEditTime: 2025-02-08 15:34:36
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM –
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -15,42 +14,37 @@
  */
 // https://www.npmjs.com/package/electron-store
 // https://github.com/sindresorhus/electron-store
-import Store from "electron-store";
-import { IS_APP_QUIT } from "../utils/constants";
+// import Store from "electron-store";
+// import { IS_APP_QUIT } from "../utils/constants";
+
+// const store = new Store();
+let isAppQuittingInstance = false;
 
 export const setIsAppQuitting = (isAppQuitting: boolean) => {
   console.log("setIsAppQuitting", isAppQuitting);
-  const store = getStoreManager();
-  store.set(IS_APP_QUIT, isAppQuitting);
+  isAppQuittingInstance = isAppQuitting;
 };
 
 export const getIsAppQuitting = () => {
-  const store = getStoreManager();
-  return store.get(IS_APP_QUIT);
+  return isAppQuittingInstance;
 };
 
+// export const getStoreManager = () : Store => {
+//   return store;
+// };
 
-export const getStoreManager = () : Store => {
-  const store = new Store();
-  return store;
-};
+// export const setStoreValue = (key: string, value: any) => {
+//   store.set(key, value);
+// };
 
-export const setStoreValue = (key: string, value: never) => {
-  const store = getStoreManager();
-  store.set(key, value);
-};
+// export const getStoreValue = (key: string) => {
+//   return store.get(key);
+// };
 
-export const getStoreValue = (key: string) => {
-  const store = getStoreManager();
-  return store.get(key);
-};
+// export const removeStoreValue = (key: string) => {
+//   store.delete(key);
+// };
 
-export const removeStoreValue = (key: string) => {
-  const store = getStoreManager();
-  store.delete(key);
-};
-
-export const clearStore = () => {
-  const store = getStoreManager();
-  store.clear();
-};
+// export const clearStore = () => {
+//   store.clear();
+// };
