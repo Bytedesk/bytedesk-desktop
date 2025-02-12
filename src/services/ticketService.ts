1416178ceeaa1c5c2ab2ab6fbd837e0da85e5bb5
jack ning <github@bytedesk.com>
2025-02-12 08:55:50 +0800

    sync: desktop update 2025-02-13 14:42:36

--- src/services/ticketService.ts
+++ src/services/ticketService.ts
@@ -3,7 +3,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-02-12 15:16:25
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-13 14:34:40
+ * @LastEditTime: 2025-02-13 14:42:28
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -168,6 +168,8 @@ export const ticketService = {
           return tryLoad(attempt + 1);
         }
         setError(error instanceof Error ? error.message : 'Failed to load tickets');
+      } finally {
+        setLoading(false);
       }
     }
 
