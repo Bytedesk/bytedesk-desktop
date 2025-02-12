7ed4e25af43c8d1d9bbdabab3ad05860b1233359
jack ning <github@bytedesk.com>
2025-02-12 08:55:50 +0800

    sync: desktop update 2025-02-13 17:54:57

--- src/apis/ticket/ticket.ts
+++ src/apis/ticket/ticket.ts
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-02-06 11:07:09
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-12 14:54:28
+ * @LastEditTime: 2025-02-13 17:50:14
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM –
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -27,8 +27,9 @@ export async function queryTicketsByOrgUid(pageParam: TICKET.TicketRequest) {
   });
 }
 
-export async function queryTicketByUid(ticket: TICKET.TicketRequest) {
-  return request<TICKET.HttpResult>("/api/v1/ticket/query/uid", {
+
+export async function queryTicketByServiceThreadTopic(ticket: TICKET.TicketRequest) {
+  return request<TICKET.HttpPageResult>("/api/v1/ticket/query/service-thread-topic", {
     method: "GET",
     params: {
       ...ticket,
@@ -37,8 +38,8 @@ export async function queryTicketByUid(ticket: TICKET.TicketRequest) {
   });
 }
 
-export async function queryTicketByServiceThreadTopic(ticket: TICKET.TicketRequest) {
-  return request<TICKET.HttpPageResult>("/api/v1/ticket/query/service-thread-topic", {
+export async function queryTicketByThreadUid(ticket: TICKET.TicketRequest) {
+  return request<TICKET.HttpResult>("/api/v1/ticket/query/thread-uid", {
     method: "GET",
     params: {
       ...ticket,
