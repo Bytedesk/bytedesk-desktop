91ecd3dc33fbbc4cf083a5fd7c161b41b7d4c998
jack ning <github@bytedesk.com>
2025-02-12 08:55:50 +0800

    sync: desktop update 2025-02-13 14:37:24

--- src/stores/ticket/ticket.ts
+++ src/stores/ticket/ticket.ts
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2025-01-23 17:42:15
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-13 14:33:56
+ * @LastEditTime: 2025-02-13 14:35:05
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
@@ -52,6 +52,7 @@ interface TicketState {
   setLoading: (loading: boolean) => void;
   setError: (error: string | null) => void;
   setTickets: (tickets: TICKET.TicketResponse[]) => void;
+  setHistoryTickets: (tickets: TICKET.TicketResponse[]) => void;
 }
 
 export const useTicketStore = create<TicketState>((set) => {
