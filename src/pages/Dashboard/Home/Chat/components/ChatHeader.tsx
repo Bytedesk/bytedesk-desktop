701d88a8b5bdcaeb357fd70c6836071f35c45d50
jack ning <github@bytedesk.com>
2025-02-12 08:55:50 +0800

    sync: desktop update 2025-02-13 23:34:27

--- src/pages/Dashboard/Home/Chat/components/ChatHeader.tsx
+++ src/pages/Dashboard/Home/Chat/components/ChatHeader.tsx
@@ -156,7 +156,9 @@ const ChatHeader = ({
               )}
             </div>
           )}
-          {(isGroupThread(currentThread) || isMemberThread(currentThread) || isRobotThread(currentThread)) && (
+          {(isGroupThread(currentThread) 
+            || isMemberThread(currentThread) 
+            || isRobotThread(currentThread)) && (
             <div
               style={{
                 display: "flex",
