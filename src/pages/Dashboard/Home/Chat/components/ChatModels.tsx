b81c8c93ce83d0b85be97442dd5db893a501c453
jack ning <github@bytedesk.com>
2025-02-12 08:55:50 +0800

    sync: desktop update 2025-02-13 20:48:19

--- src/pages/Dashboard/Home/Chat/components/ChatModels.tsx
+++ src/pages/Dashboard/Home/Chat/components/ChatModels.tsx
@@ -47,7 +47,6 @@ const ChatModels = ({
     isTicketCreateModelOpen, handleTicketCreateModelSuccess, handleTicketCreateModelCancel, 
     isBlockModelOpen, handleBlockModelOk, handleBlockModelCancel, 
     isWebRtcModelOpen, handleWebRtcModelOk, handleWebRtcModelCancel,  
-    isRobotInfoDrawerOpen, setIsRobotInfoDrawerOpen, showEmoji, handleEmojiSelect, setShowEmoji 
   }: ChatModelsProps) => {
   return (
     <>
