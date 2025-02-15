331c26e0dd38d7262038b3080b6be47634d143d2
jack ning <github@bytedesk.com>
2025-02-15 14:25:55 +0800

    sync: desktop update 2025-02-15 16:57:23

--- src/@types/ticket/ticket.d.ts
+++ src/@types/ticket/ticket.d.ts
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-02-21 15:36:09
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-12 14:47:39
+ * @LastEditTime: 2025-02-15 16:57:06
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM –
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -92,11 +92,11 @@ declare namespace TICKET {
     workgroup?: USER.UserProtobuf;
     assignee?: USER.UserProtobuf;
     reporter?: USER.UserProtobuf;
-    //
-    createdAt: string;
-    updatedAt: string;
+    
     // 
     attachments?: TICKET.TicketAttachmentResponse[];
+    processInstanceId?: string;
+    processEntityUid?: string;
   };
 
   type TicketAttachmentResponse = {
