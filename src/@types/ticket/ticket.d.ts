d538368fefb114a30e4a5502d19d903c32cceb2c
jack ning <github@bytedesk.com>
2025-02-18 18:22:11 +0800

    sync: desktop update 2025-02-19 07:44:05

--- src/@types/ticket/ticket.d.ts
+++ src/@types/ticket/ticket.d.ts
@@ -3,7 +3,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-02-21 15:36:09
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-19 07:36:56
+ * @LastEditTime: 2025-02-19 07:42:43
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM –
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -163,25 +163,33 @@ declare namespace TICKET {
   };
 
   type TicketHistoryTaskResponse = {
-    processInstanceId?: string;      // 流程实例ID
-    processDefinitionId?: string;    // 流程定义ID
-    processDefinitionName?: string;  // 流程定义名称
-    processDefinitionKey?: string;   // 流程定义Key
-    processDefinitionVersion?: number; // 流程定义版本
-    businessKey?: string;            // 业务键(ticketUid)
-    startUserId?: string;           // 发起人ID
-    startTime?: Date;               // 开始时间
-    endTime?: Date;                 // 结束时间
-    durationInMillis?: number;        // 持续时间(毫秒)
-    deleteReason?: string;          // 删除原因
-    tenantId?: string;              // 租户ID
-    name?: string;                  // 流程名称
-    description?: string;           // 描述
-    status?: string;                // 状态
-    // 
-    assignee?: USER.UserProtobuf;              // 分配给谁
-    priority?: string;              // 优先级
-    categoryUid?: string;           // 分类UID
+    taskId?: string;                 // 任务ID
+    taskName?: string;               // 任务名称
+    taskDefinitionKey?: string;      // 任务定义Key
+    taskDefinitionId?: string;       // 任务定义ID
+    description?: string;            // 任务描述
+    category?: string;               // 任务类别
+    formKey?: string;                // 表单Key
+    processInstanceId?: string;      // 所属流程实例ID
+    processDefinitionId?: string;    // 所属流程定义ID
+    executionId?: string;            // 执行实例ID
+    
+    assignee?: string;               // 任务处理人
+    owner?: string;                  // 任务所有者
+    candidateUsers?: string[];   // 候选用户列表
+    candidateGroups?: string[];  // 候选组列表
+    
+    priority?: number;              // 优先级
+    createTime?: Date;               // 创建时间
+    dueDate?: Date;                  // 截止时间
+    claimTime?: Date;                // 认领时间
+    endTime?: Date;                  // 完成时间
+    durationInMillis?: number;         // 持续时间(毫秒)
+    deleteReason?: string;           // 删除原因
+    tenantId?: string;               // 租户ID
+    
+    taskLocalVariables?: Record<string, any>;    // 任务局部变量
+    processVariables?: Record<string, any>;      // 流程变量
   };
 
   // 工单活动历史
