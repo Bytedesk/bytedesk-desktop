7efef7da7be38efc65d0a38d55bf389dd83c6c63
jack ning <github@bytedesk.com>
2025-02-19 12:37:10 +0800

    sync: desktop update 2025-02-19 20:01:51

--- src/pages/Dashboard/Home/Chat/components/ChatModels.tsx
+++ src/pages/Dashboard/Home/Chat/components/ChatModels.tsx
@@ -21,7 +21,7 @@ import HistoryMessageModel from "@/components/Vip/HistoryMessageModel";
 import TransferMessageModel from "@/components/Vip/TransferMessageModel";
 import TransferThreadModel from "@/components/Vip/TransferThreadModel";
 import WebRtcModel from "@/components/Vip/WebRtcModel";
-import TicketCreateDrawer from "@/pages/Vip/Ticket/components/TicketCreateDrawer";
+import TicketCreateDrawer from "@/pages/Vip/Ticket/components/TicketDrawer";
 import EmojiPicker from "@emoji-mart/react";
 import GroupInfoDrawer from "../../RightPanel/GroupInfo";
 import MemberInfoDrawer from "../../RightPanel/MemberInfo";
