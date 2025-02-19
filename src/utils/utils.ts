aa7513a04bb9a39bdc101f78834b8799510c337e
jack ning <github@bytedesk.com>
2025-02-19 12:37:10 +0800

    sync: desktop update 2025-02-19 18:44:57

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,7 +2,7 @@
  * @Author: jackning 270580156@qq.com
  * @Date: 2024-02-23 22:36:47
  * @LastEditors: jackning 270580156@qq.com
- * @LastEditTime: 2025-02-19 17:52:06
+ * @LastEditTime: 2025-02-19 18:44:41
  * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
  *   Please be aware of the BSL license restrictions before installing Bytedesk IM –
  *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license.
@@ -373,6 +373,8 @@ export function getTicketPriorityColor(priority: string) {
       return "default";
   }
 }
+
+// 判断当前工单是否
 
 // 判断当前工单是否是自己的工单
 export const canChat = (
