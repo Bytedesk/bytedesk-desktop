0a95d9aed0d01008091ed427474968a08e1fcbc8
jack ning <github@bytedesk.com>
2025-02-19 12:17:49 +0800

    sync: desktop update 2025-02-19 16:52:37

--- src/pages/Dashboard/Home/Chat/index.tsx
+++ src/pages/Dashboard/Home/Chat/index.tsx
@@ -204,7 +204,7 @@ const ChatPage = ({ fromTicketTab = false, ticket }: ChatPageProps) => {
   // console.log("ChatPage fromTicketTab:", fromTicketTab, chatThread);
 
   const fetchTicketThread = async (threadUid?: string) => {
-    console.log('')
+    console.log('ChatPage fetchTicketThread')
     if (!threadUid) {
       console.log("ChatPage fetchTicketThread: threadUid is undefined");
       // 待认领状态，设置当前会话为空，清空聊天记录
