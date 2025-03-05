144121ffb16a772f425d179c67bcbf5ffdbb9d85
jack ning <github@bytedesk.com>
2025-03-05 16:23:14 +0800

    sync: desktop update 2025-03-05 16:23:14

--- src/locales/en-US/customer.ts
+++ src/locales/en-US/customer.ts
@@ -32,11 +32,19 @@ export default {
   'black.reason.required': 'Please enter block reason',
   'black.reason.placeholder': 'Enter reason for blocking',
   'black.success': 'Block successfully',
+
+  'customer.info.basic': 'Basic Info',
+  'customer.info.browser': 'Browser Info',
+  'customer.info.os': 'OS Info',
+  'customer.info.device': 'Device Info',
+  'customer.info.browse.record': 'Browse Record',
+  'customer.info.tag': 'Tag Info',
+  'customer.info.load.error': 'Failed to load visitor info',
+  'customer.basic.title': 'Basic Information',
+  'customer.basic.nickname': 'Nickname',
+  'customer.basic.empty': 'N/A',
+  'customer.basic.edit': 'Edit',
+  'customer.basic.update.success': 'Updated successfully',
+  'customer.basic.update.failed': 'Update failed',
+  'customer.basic.note': 'Note'
 }
-
-// 补充或修改缺失的翻译
-const messages = {
-  
-};
-
-export default messages;
