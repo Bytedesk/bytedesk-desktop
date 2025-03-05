1fa6a183aa63fd353f11573099749d4b5ba11740
jack ning <github@bytedesk.com>
2025-03-05 09:52:19 +0800

    sync: desktop update 2025-03-05 16:22:44

--- src/locales/en-US/customer.ts
+++ src/locales/en-US/customer.ts
@@ -1,3 +1,17 @@
+/*
+ * @Author: jackning 270580156@qq.com
+ * @Date: 2025-02-03 23:01:47
+ * @LastEditors: jackning 270580156@qq.com
+ * @LastEditTime: 2025-03-05 16:22:43
+ * @Description: bytedesk.com https://github.com/Bytedesk/bytedesk
+ *   Please be aware of the BSL license restrictions before installing Bytedesk IM – 
+ *  selling, reselling, or hosting Bytedesk IM as a service is a breach of the terms and automatically terminates your rights under the license. 
+ *  仅支持企业内部员工自用，严禁私自用于销售、二次销售或者部署SaaS方式销售 
+ *  Business Source License 1.1: https://github.com/Bytedesk/bytedesk/blob/main/LICENSE 
+ *  contact: 270580156@qq.com 
+ * 联系：270580156@qq.com
+ * Copyright (c) 2025 by bytedesk.com, All Rights Reserved. 
+ */
 export default {
   // Customer Info
   'customer.info.basic': 'Basic Info',
@@ -31,4 +45,24 @@ export default {
   'black.reason.required': 'Please enter block reason',
   'black.reason.placeholder': 'Enter reason for blocking',
   'black.success': 'Block successfully'
-} 
+}
+
+// 补充或修改缺失的翻译
+const messages = {
+  'customer.info.basic': 'Basic Info',
+  'customer.info.browser': 'Browser Info',
+  'customer.info.os': 'OS Info',
+  'customer.info.device': 'Device Info',
+  'customer.info.browse.record': 'Browse Record',
+  'customer.info.tag': 'Tag Info',
+  'customer.info.load.error': 'Failed to load visitor info',
+  'customer.basic.title': 'Basic Information',
+  'customer.basic.nickname': 'Nickname',
+  'customer.basic.empty': 'N/A',
+  'customer.basic.edit': 'Edit',
+  'customer.basic.update.success': 'Updated successfully',
+  'customer.basic.update.failed': 'Update failed',
+  'customer.basic.note': 'Note'
+};
+
+export default messages;
