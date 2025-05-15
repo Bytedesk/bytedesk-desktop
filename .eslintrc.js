969bc27fef1c1037ee17a95c52052fcd4f9928a6
jack ning <github@bytedesk.com>
2025-05-15 21:08:31 +0800

    sync: desktop update 2025-05-15 21:38:33

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // extends: [require.resolve("@umijs/lint/dist/config/eslint")],
+  globals: {
+    page: true,
+    REACT_APP_ENV: true,
+    webkitIsFullScreen: true,
+    mozRequestFullScreen: true,
+    webkitRequestFullScreen: true,
+    msRequestFullscreen: true,
+    exitFullScreen: true,
+    mozExitFullScreen: true,
+    webkitExitFullscreen: true,
+    msExitFullscreen: true,
+  },
+  plugins: ["import"],
+  rules: {
+    // Note: you must disable the base rule as it can report incorrect errors
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "import/no-unused-modules": "off",
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": "off"
+  },
+};
